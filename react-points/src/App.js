1cf5e92cc25a1954ff018dec30baff57110c63a0
sitota49 <se.sitota.ezra@gmail.com>
2022-02-13 16:45:27 +0300

    react points for locations around Addis Ababa

---
+++ react-points/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div id="viewDiv"></div>
+     <h1>hello</h1>
+    </div>
+  );
+}
+
+export default App;
